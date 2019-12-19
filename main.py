abb3b3f2d64094bdddca37c1d4ea6502fe1ac241
leeahwi <48510004+leeahwi@users.noreply.github.com>
2019-12-19 11:47:14 +0900

    Update main.py

--- main.py
+++ main.py
@@ -17,20 +17,4 @@ async def on_ready():
     print('Username: ' + str(client.user.name))
     print('Client ID: ' + str(client.user.id))
 
-
-@bot.command()
-async def join():
- if message.author.voice_channel != None and client.is_voice_connected(message.server) != True:
-                global currentChannel
-                global player
-                global voice
-                currentChannel = client.get_channel(message.author.voice_channel.id)
-                voice = await client.join_voice_channel(currentChannel)
-
-            elif message.author.voice_channel == None:
-                await client.send_message(message.channel, '절 부른 사람이 음성채널에 없어요..')
-
-            else:
-                await client.send_message(message.channel, '이미 들왔어!')
-
 client.run(TOKEN)
