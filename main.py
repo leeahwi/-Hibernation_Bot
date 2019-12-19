a156b249599f8d4e71c74e1ddd35cca563a64f6b
leeahwi <48510004+leeahwi@users.noreply.github.com>
2019-12-19 12:04:44 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,15 +1,9 @@
 import discord
-import asyncio
-##import youtube_dl
-##import requests
 import os
 
-from features import *
+client = discord.Client()
 
 TOKEN = os.environ('BOT_TOKEN')
-
-client = discord.Client()
-bot = commands.Bot(command_prefix='#')
 
 @client.event
 async def on_ready():
@@ -17,4 +11,4 @@ async def on_ready():
     print('Username: ' + str(client.user.name))
     print('Client ID: ' + str(client.user.id))
 
-client.run(TOKEN)
+client(TOKEN)
