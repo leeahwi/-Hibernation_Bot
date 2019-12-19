cc3ac9d6e2ee59e0a73fd6a9db37a06b122704db
leeahwi <48510004+leeahwi@users.noreply.github.com>
2019-12-19 12:20:00 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,21 @@
 import discord
 import os
 
+from discord.ext import commands
+
 client = discord.Client()
 
 TOKEN = os.environ['BOT_TOKEN']
+bot = commands.Bot(command_prefix='$')
 
 @client.event
 async def on_ready():
     print('Logged in...')
     print('Username: ' + str(client.user.name))
     print('Client ID: ' + str(client.user.id))
+
+@bot.command
+async def test():
+  await message.channel.send("test")
 
 client.run(TOKEN)
